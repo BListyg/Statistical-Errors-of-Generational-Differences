bbc4af3d96cb42dca214281e3ef8d9103427ce79
Benjamin Listyg <listyg.ben@gmail.com>
2018-04-16 15:02:38 -0500

    Update normal_age_dist.R

--- normal_age_dist.R
+++ normal_age_dist.R
@@ -61,7 +61,7 @@ agesim <- function(x,y){
   
   #Adding in measurement error as a N ~ (0,1) rv to the original Y true score (not rescaled yet) variable
    
-  age_data$entitlement <- round(scales::rescale(age_data$Y, to = c(1,5)),0)
+  age_data$entitlement <- round(scales::rescale(age_data$Y, to = c(10,50)),0)
   
   #Creating generational bins how it's done now
   age_data$gen1[age_data$age<=35] <- "Millenial"
