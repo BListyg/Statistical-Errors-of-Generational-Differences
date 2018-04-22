9f215848c0b4a2bcb1ac582fd4d1ecd7c47db942
Benjamin Listyg <listyg.ben@gmail.com>
2018-04-22 12:58:30 -0500

    Update normal_age_dist.R

--- normal_age_dist.R
+++ normal_age_dist.R
@@ -132,9 +132,9 @@ axis(side = 1, at = seq(0,9,1), label = seq(0.0,0.9,.1))
 
 title(main = "Age Normally Distributed w/i sample \nAge negatively related w/ Entitlement \nn = 300, 50 repititions", sub = "R^2")
 
-matlines(b, type = "c", ylim = c(0,1), col = "black")
+matlines(b, type = "c", ylim = c(0,5), col = "black")
 
-matlines(c, type = "c", ylim = c(0,1), col = "black")
+matlines(c, type = "c", ylim = c(0,5), col = "black")
 
 matlines(rowMeans(a), type = "s", lwd = 5, col = "yellow")
 
